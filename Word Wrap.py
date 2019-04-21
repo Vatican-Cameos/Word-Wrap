cedd346de6532d90335dfc8cfd2b043e0b693213
Pavan <pavanvenkatesh94@gmail.com>
2019-04-21 00:30:11 -0700

    :sparkles: Text is now read from file - Writes result to a file - glitchy

--- Word Wrap.py
+++ Word Wrap.py
@@ -1,4 +1,3 @@
-from array import *
 
 def prepareLineCostTable(sample, lineWidth):
     print("Preparing Line Costs")
@@ -30,23 +29,56 @@ def getWrappedTextAssignment(lineCostLookUp):
     size = len(sample.split(" "))
     opt = [0] * (size + 1)
     assignmentIndex = [0] * (size + 1)
-    breakWordIndex = []
-    print(lineCostLookUp)
+    resultIndex = [0] * (size + 1)
+    #print(lineCostLookUp)
     opt[0] = 0
     assignmentIndex[0] = 0
+    resultIndex[0] = 0
     for i in range(1,size+1):
         min = 99999999
         for j in range(1,i+1):
             temp = opt[j-1] + lineCostLookUp[j-1][i-1]
             if(temp < min):
                 min = temp
+                resultIndex[i] = j-1
         opt[i] = min
-    return opt
+        print(opt)
+    return resultIndex
 
 if __name__ == '__main__':
-    sample = "Who is it"
-    lineWidth = 6
+    File_object = open(r"test.txt", "r")
+    sample = File_object.read()
+    File_object.close()
+    print(sample)
+    #sample = "Who is it and what are you"
+
+    lineWidth = 15
     lineCostLookUp = prepareLineCostTable(sample,lineWidth)
     #print(lineCostLookUp)
     assignment = getWrappedTextAssignment(lineCostLookUp)
-    print(assignment)
+    result = [0] * (len(assignment))
+    for i in range(1,len(assignment)):
+        result[i] = result[assignment[i]] + 1
+
+    lineNo = 1
+    stringNew = ''
+    index = 1
+
+    for word in sample.split():
+        wordIndex = result[index]
+        if(lineNo == wordIndex):
+            stringNew += word + ' '
+        else:
+            lineNo = result[index]
+            stringNew += '\n' + word + ' '
+        index += 1
+
+    print(stringNew)
+    File_object = open(r"formatted.txt", "w")
+    File_object.write(stringNew)
+    File_object.close()
+    print(result)
+
+
+def insert_str(string, str_to_insert, index):
+    return string[:index] + str_to_insert + string[index:]
